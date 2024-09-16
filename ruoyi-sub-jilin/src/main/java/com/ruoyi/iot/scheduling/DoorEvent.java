fc3b2406f66ccc80f2fd185cbd6a0f02ba2001e6
y <q15081006783@163.com>
2024-09-16 22:12:58 +0800

    图片显示

--- ruoyi-sub-jilin/src/main/java/com/ruoyi/iot/scheduling/DoorEvent.java
+++ ruoyi-sub-jilin/src/main/java/com/ruoyi/iot/scheduling/DoorEvent.java
@@ -98,7 +98,7 @@ public class DoorEvent {
             map.put("portal_id", "1751847977770553345");
             map.put("device_code", object.get("doorIndexCode").toString());//
             map.put("device_status", "在线");
-            String url = "http://192.168.30.151" + object.get("picUri");
+            String url = "http://10.1.3.2" + object.get("picUri");
             map.put("record_Image_file", url);
             map.put("id_card", object.get("certNo"));
             map.put("in_out_direction", object.get("inAndOutType").toString().equals("1") ? "进" : "出");

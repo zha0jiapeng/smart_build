1d83d51d03d67375474005e3ef5d24ed1a145d82
y <q15081006783@163.com>
2024-09-16 21:59:05 +0800

    修改tsp接口类型

--- ruoyi-admin/src/main/java/com/ruoyi/web/controller/basic/view/hardware/TspController.java
+++ ruoyi-admin/src/main/java/com/ruoyi/web/controller/basic/view/hardware/TspController.java
@@ -16,10 +16,7 @@ import com.ruoyi.system.service.MerchantService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.util.CollectionUtils;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 import java.util.*;
@@ -43,7 +40,7 @@ public class TspController extends BaseController {
         return getDataTable(iotTspList);
     }
 
-    @PostMapping("bim/tsp/list")
+    @RequestMapping("bim/tsp/list")
     public TableDataInfo queryByPageBim() {
         List<IotTsp> iotTspLists = new ArrayList<>();
         QueryWrapper<IotTsp> queryWrapper = new QueryWrapper<>();

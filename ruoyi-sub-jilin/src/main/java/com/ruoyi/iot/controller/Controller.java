a33f66bcd519bb727f4f380d297c83d433020d02
赵佳鹏 <1939291579@qq.com>
2024-09-26 09:00:21 +0800

    aa

--- ruoyi-sub-jilin/src/main/java/com/ruoyi/iot/controller/Controller.java
+++ ruoyi-sub-jilin/src/main/java/com/ruoyi/iot/controller/Controller.java
@@ -91,7 +91,7 @@ public class Controller {
             }
             Map<String, Object> map = new HashMap<>();
             map.put("portal_id", "1751847977770553345");
-            map.put("device_code", object.get("doorIndL:exCode").toString());//
+            map.put("device_code", object.get("doorIndexCode").toString());//
             map.put("device_status", "在线");
             String url = "http://10.1.3.2" + object.get("picUri");
             map.put("record_Image_file", url);

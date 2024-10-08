704434011b48761779b9123d70ed2e96581c7691
y <q15081006783@163.com>
2024-10-08 11:28:36 +0800

    用电监测,

--- ruoyi-sub-jilin/src/main/java/com/ruoyi/iot/scheduling/MqttSubscriber.java
+++ ruoyi-sub-jilin/src/main/java/com/ruoyi/iot/scheduling/MqttSubscriber.java
@@ -3,10 +3,15 @@ package com.ruoyi.iot.scheduling;
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
+import com.ruoyi.iot.domain.ElectricityMonitoring;
+import com.ruoyi.iot.service.IElectricityMonitoringService;
 import com.ruoyi.iot.utils.HdyHttpUtils;
 import org.eclipse.paho.client.mqttv3.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.PostConstruct;
 import javax.annotation.Resource;
 import java.text.DecimalFormat;
 import java.text.SimpleDateFormat;
@@ -16,12 +21,16 @@ import java.util.*;
  * @author y
  */
 @Service
+@Component
 public class MqttSubscriber {
+
+    @Autowired
+    private IElectricityMonitoringService electricityMonitoringService;
 
     @Resource
     HdyHttpUtils hdyHttpUtils;
 
-
+    @PostConstruct
     public void subscribe() {
         // MQTT 代理地址
         String broker = "tcp://10.1.3.234:1883";
@@ -67,12 +76,15 @@ public class MqttSubscriber {
 
     // 添加一个静态变量用于计数
     private static int callCount = 0;
+    private static Map<Integer, String> idValueMap = new HashMap<>();
+    private static String rawData = "";
 
     private void parseMessage(String json) {
-        Map<Integer, String> idValueMap = new HashMap<>();
         boolean isConfirm = false;
         try {
+            rawData += json;
             System.out.println("初始数据：" + json);
+
             com.alibaba.fastjson.JSONObject jsonObject = JSON.parseObject(json);
 
             if (jsonObject.containsKey("data")) {
@@ -102,12 +114,17 @@ public class MqttSubscriber {
 
         // 当第二次调用并且满足条件时才调用 pushIOT
         if (callCount == 2 && isConfirm) {
-            pushIOT(idValueMap);
-            callCount = 0; // 重置计数器
+            ElectricityMonitoring electricityMonitoring = new ElectricityMonitoring();
+            electricityMonitoring.setRawData(rawData);
+            pushIOT(idValueMap, electricityMonitoring);
+            idValueMap = new HashMap<>();
+            rawData = "";
+            // 重置计数器
+            callCount = 0;
         }
     }
 
-    public void pushIOT(Map<Integer, String> idValueMap) {
+    public void pushIOT(Map<Integer, String> idValueMap, ElectricityMonitoring electricityMonitoring) {
         Map<String, Object> valueMap = new HashMap<>();
         //门户ID  String
         valueMap.put("portal_id", "1751847977770553345");
@@ -123,45 +140,62 @@ public class MqttSubscriber {
         valueMap.put("electric_box_parent", "");
         //正向无功总电能  Decimal 30
         valueMap.put("total_p_r_e", idValueMap.get(30));
+        electricityMonitoring.setTotalPRE(idValueMap.get(30));
         //反向无功总电能  Decimal 31
         valueMap.put("total_p_r_r_e", idValueMap.get(31));
+        electricityMonitoring.setTotalPRRE(idValueMap.get(31));
         //正向有功总电量  Decimal 28
         valueMap.put("total_p_a_e", idValueMap.get(28));
+        electricityMonitoring.setTotalPAE(idValueMap.get(28));
         //反向有功总电量  Decimal 29
         valueMap.put("total_p_r_a_e", idValueMap.get(29));
+        electricityMonitoring.setTotalPRAE(idValueMap.get(29));
         //ABC三项电压  Decimal 1+2+3
         String a_b_c_voltage = calculateAndFormatSum(idValueMap, 1, 2, 3);
         valueMap.put("a_b_c_voltage", a_b_c_voltage);
+        electricityMonitoring.setABCVoltage(a_b_c_voltage);
         //ABC三项电流  Decimal (7+8+9)/3
         String a_b_c_currentString = calculateAndFormatSum(idValueMap, 1, 2, 3);
-        Double a_b_c_currentDouble = Double.parseDouble(a_b_c_currentString)/3;
+        Double a_b_c_currentDouble = Double.parseDouble(a_b_c_currentString) / 3;
         Double result = a_b_c_currentDouble / 3;
         // 格式化结果保留一位小数
         DecimalFormat decimalFormat = new DecimalFormat("#.#");
         result = Double.valueOf(decimalFormat.format(result));
         valueMap.put("a_b_c_current", String.valueOf(result));
+        electricityMonitoring.setABCCurrent(String.valueOf(result));
         //A相电流  Decimal 7
         valueMap.put("a_electricity", idValueMap.get(7));
+        electricityMonitoring.setAElectricity(idValueMap.get(7));
         //B相电流  Decimal 8
         valueMap.put("b_blectricity", idValueMap.get(8));
+        electricityMonitoring.setBBlectricity(idValueMap.get(8));
         //C相电流  Decimal 9
         valueMap.put("c_electricity", idValueMap.get(9));
+        electricityMonitoring.setCElectricity(idValueMap.get(9));
         //A相电压  Decimal 1
         valueMap.put("a_voltage", idValueMap.get(1));
+        electricityMonitoring.setAVoltage(idValueMap.get(1));
         //B相电压  Decimal 2
         valueMap.put("b_voltage", idValueMap.get(2));
+        electricityMonitoring.setBVoltage(idValueMap.get(2));
         //C相电压  Decimal 3
         valueMap.put("c_voltage", idValueMap.get(3));
+        electricityMonitoring.setCVoltage(idValueMap.get(3));
         //用电负载功率  Decimal
         valueMap.put("box_power", "");
         //线缆A温度  Decimal 36
         valueMap.put("line_a_temperature", idValueMap.get(36));
+        electricityMonitoring.setLineATemperature(idValueMap.get(36));
         //线缆B温度  Decimal 37
         valueMap.put("line_b_temperature", idValueMap.get(37));
+        electricityMonitoring.setLineBTemperature(idValueMap.get(37));
         //线缆C温度  Decimal 38
         valueMap.put("line_c_temperature", idValueMap.get(38));
+        electricityMonitoring.setLineCTemperature(idValueMap.get(38));
         //线缆N温度  Decimal 39
         valueMap.put("line_n_temperature", idValueMap.get(39));
+        electricityMonitoring.setLineNTemperature(idValueMap.get(39));
+
         //总瞬时功率  Decimal
         valueMap.put("totalI_a_p", "");
         //跳闸  String
@@ -198,8 +232,9 @@ public class MqttSubscriber {
         values.add(valueMap);
         Map<String, List<Map<String, Object>>> param = new HashMap<>();
         param.put("values", values);
-
-        hdyHttpUtils.pushIOT(param, "2e2529ef-f03a-4159-8ebb-a050e0e0fc89");
+        //开始插入数据库
+        electricityMonitoringService.insertElectricityMonitoring(electricityMonitoring);
+        hdyHttpUtils.pushIOT(param, "query/2e2529ef-f03a-4159-8ebb-a050e0e0fc89");
     }
 
 
